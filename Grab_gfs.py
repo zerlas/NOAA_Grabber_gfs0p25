45eb416004bdd7b341e073a6e3111d255d09567c
croise_pe <pierre-edouard.croise@epitech.eu>
2018-06-29 11:58:31 +0200

    Changing 2 strings for variable and level into one double string [variable, level]

--- Grab_gfs.py
+++ Grab_gfs.py
@@ -8,12 +8,8 @@ SAVE_DIR = "./NOAA_Files/"
 if not os.path.exists(SAVE_DIR):
     os.makedirs(SAVE_DIR)
 
-# the GFS fetch configuration: levels & vars
-# each one is an array, each element of an array is mapping to same element in the another array
-# ex: levels[0] is mapping to vars[0], levels[1] is mapping to vars[1] ...
-# THE LENGTH OF THESE TWO ARRAYS MUST BE THE SAME!
-levels = ['10_m_above_ground', '10_m_above_ground', '2_m_above_ground', 'mean_sea_level']
-variables = ['UGRD', 'VGRD', 'TMP', 'PRMSL']
+# the GFS fetch configuration: [[Variable_1, Level], [Variable_2, Level], ...]
+value = [["UGRD", "10_m_above_ground"], ["VGRD", "10_m_above_ground"], ["TMP", "2_m_above_ground"], ["PRMSL", "mean_sea_level"]]
 
 now = datetime.now()
 date = now.strftime("%Y%m%d")
@@ -32,13 +28,15 @@ str_scan = str_scan.zfill(2)
 
 i = 0
 
-while i < len(levels):
+while i < len(value):
     forecast_hour = 0
     while forecast_hour <= 120:
         after_hour = str(forecast_hour).zfill(3)
         file_date = date + after_hour
-        file_name = "".join(["gfs-", file_date, "-", levels[i], "-", variables[i]])
-        url = "".join(["http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t", str_scan, "z.pgrb2.0p25.f", after_hour, "&lev_", levels[i], "=on&var_", variables[i], "=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.", date, str_scan])
+        file_name = "".join(["gfs-", file_date, "-", value[i][1], "-", value[i][0]])
+        print(value[i][0], value[i][1])
+        url = "".join(["http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl?file=gfs.t", str_scan, "z.pgrb2.0p25.f", after_hour, "&lev_", value[i][1], "=on&var_", value[i][0], "=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.", date, str_scan])
+        print(url)
         print("Downloading", file_name)
         directory = "".join([SAVE_DIR, file_name])
         urllib.request.urlretrieve(url, directory)
